dc2b5879f8b18f54451c77af2e6a99e7095a15c6
LltQ <lingloktin@gmail.com>
2024-04-12 01:38:18 +0800

    sample API

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Product = require('./models/product.model')
+const config = require('./config')
 const app = express()
-const connectionString = require('./connectionString')
 
-
-
+// make this server accept json data
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send("hello 3001");    
+    res.send("this is a response from the server for get request");    
 });
 
-mongoose.connect(connectionString)
+// app.post('/test-post', (req, res) => {
+//     console.log(req.body)
+//     res.send(req.body)
+// })
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/api/products/:id', async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+
+})
+
+mongoose.connect(config.connectionString)
     .then(() => {
         console.log("Connected to MongoDB");
         // listen to port 3000
@@ -21,4 +54,4 @@ mongoose.connect(connectionString)
     .catch((err) => {
         console.log("Error connecting to MongoDB");
         console.log(err);
-    })
+    })  
